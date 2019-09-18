151d536639b5764092e7fdade6460d936a836252
Kamil Chyb <kamilchyb@gmail.com>
2019-09-18 12:22:51 +0100

    Remove comment from app.js

--- weather-app/src/components/App.js
+++ weather-app/src/components/App.js
@@ -24,39 +24,6 @@ class App extends Component {
       value: e.target.value
     });
   };
-  // handleCitySubmit = e => {
-  //   e.preventDefault();
-
-  //   const API = `https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${APIKey}&units=metric`;
-  //   fetch(API)
-  //     .then(response => {
-  //       if (response.ok) {
-  //         return response;
-  //       }
-  //       throw Error("Nie udało się");
-  //     })
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       const time = new Date().toLocaleString();
-  //       this.setState(prevState => ({
-  //         err: false,
-  //         date: time,
-  //         sunrise: data.sys.sunrise,
-  //         sunset: data.sys.sunset,
-  //         temp: data.main.temp,
-  //         pressure: data.main.pressure,
-  //         wind: data.wind.speed,
-  //         city: prevState.value
-  //       }));
-  //     })
-  //     .catch(err => {
-  //       console.log(err);
-  //       this.setState(prevState => ({
-  //         err: true,
-  //         city: prevState.value
-  //       }));
-  //     });
-  // };
 
   componentDidUpdate(prevProps, prevState) {
     if (this.state.value.length === 0) return;
